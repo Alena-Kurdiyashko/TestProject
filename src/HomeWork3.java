7b19b241c7ec3a2dd2f334f5e886d2f097f61148
geekbrains <alesikxxx@mail.ru>
2022-03-17 02:24:53 +0300

    Выполненное третье дз

---
+++ src/HomeWork3.java
@@ -0,0 +1,59 @@
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+        fillDiagonal();
+
+    }
+
+    public static void invertArray() {
+        int [] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 1) {
+                arr[i] = 0;
+            } else arr[i] = 1;
+        }
+
+
+    }
+
+    public static void fillArray() {
+        int [] arr = new int[100];
+        for( int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+    }
+
+    public static void changeArray() {
+        int [] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9 };
+        for( int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+
+
+        }
+    }
+    public static void fillDiagonal() {
+        int [][] arr = new int [9][9];
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                if (j == arr[i].length - 1 - i) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr [i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void some(int len, int initialValue){
+        int [] arr = new int[len];
+        for (int i = 0; i < arr.length; i++){
+            arr [i] = initialValue;
+        }
+    }
+}
